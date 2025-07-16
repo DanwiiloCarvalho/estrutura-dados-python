e18430d938eb674121324bf10c03329c566e7aba
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-07-16 18:25:43 -0300

    feat: implement next property with getter and setter for Node class

--- singly_linked_list/node.py
+++ singly_linked_list/node.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
+
 class Node[T]:
     def __init__(self, data: T):
         self.__data: T = data
-        self.__next = None
+        self.__next: Optional['Node[T]'] = None
 
     @property
     def data(self) -> T:
@@ -10,3 +13,11 @@ class Node[T]:
     @data.setter
     def data(self, data: T) -> None:
         self.__data = data
+
+    @property
+    def next(self) -> Optional['Node[T]']:
+        return self.__next
+
+    @next.setter
+    def next(self, node: Optional['Node[T]']) -> None:
+        self.__next = node
